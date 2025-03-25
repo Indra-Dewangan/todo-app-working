83229b1470f2c462e280147539ceb9c005c420f9
Indra-Dewangan <indradewangan269@gmgmail.com>
2025-03-25 16:01:45 +0530

    to-do-app

---
+++ src/App.jsx
@@ -0,0 +1,76 @@
+import { useState } from 'react';
+import  './styles.css';
+
+
+function App() {
+
+  const [input , setinput] = useState("");
+  const [todos , settodos] = useState([]);
+
+  const inputhandler = (e) => {
+    setinput(e.target.value);
+    
+  }
+
+
+  const togglecompletion = (id) => {
+    settodos((currenttodos) => 
+      currenttodos.map((todo) => 
+        todo.id === id ? { ...todo, Completed : !todo.Completed  } : todo
+      )
+      
+    );
+  };
+  
+  
+
+  const submithandler = (e) => {
+    e.preventDefault();
+    settodos((currenttodos) =>{ return[...currenttodos,
+      {id : crypto.randomUUID() ,title : input, 
+        Completed : false }]})
+        setinput("");
+        if(todos.title === "") return
+
+
+
+        console.log(todos.title)
+
+  }
+
+   const deletetodo = (id) => {
+    settodos((currenttodos) => 
+      currenttodos.filter((todo) => todo.id !==  id) 
+       
+    )
+      
+   }
+  
+  
+  console.log(todos);
+   
+  return(
+   <>
+    <form onSubmit={submithandler}>
+     
+      <label htmlFor="todo">Enter your ToDos :</label>
+      <input value={input} onChange={inputhandler} type='text' placeholder='Enter todo' id='todo'></input>
+      <button  type='submit' >Submit</button>
+    </form>
+    
+      <h1 style={{font:"icon" , fontSize:"40px"}}>Todo List:</h1>
+      {todos.map((todo) => {
+        return <ul>
+          <li key={todo.id} complete={todo.complete}> 
+            <label><input checked={todo.Completed} onChange={() => togglecompletion(todo.id)} type='checkbox'/> {todo.title} {todo.complete}</label>
+          <button onClick={() => deletetodo(todo.id)} 
+           style={{color:"red"}} > Delete</button>
+           
+           </li>
+       </ul> 
+          })}
+      
+    </>
+  )
+}
+export default App ;
